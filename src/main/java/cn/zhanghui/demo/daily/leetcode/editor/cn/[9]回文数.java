2e11b7160f34f57d0a6823027d1a7955de716b7f
huizhang43 <huizhang43@iflytek.com>
2023-04-14 11:08:17 +0800

    leetcode day and day [2023-04-14]

---
+++ src/main/java/cn/zhanghui/demo/daily/leetcode/editor/cn/[9]回文数.java
@@ -0,0 +1,95 @@
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。 
+//
+// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 
+//
+// 
+// 例如，121 是回文，而 123 不是。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 121
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = -121
+//输出：false
+//解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 10
+//输出：false
+//解释：从右向左读, 为 01 。因此它不是一个回文数。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶：你能不将整数转为字符串来解决这个问题吗？ 
+//
+// Related Topics 数学 👍 2494 👎 0
+package cn.zhanghui.demo.daily.leetcode.editor.cn;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution9 {
+
+    public boolean isPalindrome(int x) {
+        //思考：这里大家可以思考一下，为什么末尾为 0 就可以直接返回 false
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+
+    public boolean isPalindrome2(int x) {
+        //边界判断
+        if (x < 0) return false;
+        int div = 1;
+        //
+        while (x / div >= 10) div *= 10;
+        while (x > 0) {
+            int left = x / div;
+            int right = x % 10;
+            if (left != right) return false;
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome3(int x) {
+        if (x < 0) return false;
+        if (x < 10) return true;
+
+        String xStr = x + "";
+        char[] chars = xStr.toCharArray();
+        int left = 0, right = chars.length - 1;
+
+        while (left < right) {
+            if (chars[left++] != chars[right--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
