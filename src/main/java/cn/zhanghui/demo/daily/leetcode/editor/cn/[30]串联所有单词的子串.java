c4172e237b41a7f5479eb64107c656bf0c3e4dd8
huizhang43 <huizhang43@iflytek.com>
2023-05-17 13:53:40 +0800

    leetcode day and day [2023-05-17]

---
+++ src/main/java/cn/zhanghui/demo/daily/leetcode/editor/cn/[30]串联所有单词的子串.java
@@ -0,0 +1,167 @@
+//给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。 
+//
+// s 中的 串联子串 是指一个包含 words 中所有字符串以任意顺序排列连接起来的子串。 
+//
+// 
+// 例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，
+//"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。 
+// 
+//
+// 返回所有串联字串在 s 中的开始索引。你可以以 任意顺序 返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "barfoothefoobarman", words = ["foo","bar"]
+//输出：[0,9]
+//解释：因为 words.length == 2 同时 words[i].length == 3，连接的子字符串的长度必须为 6。
+//子串 "barfoo" 开始位置是 0。它是 words 中以 ["bar","foo"] 顺序排列的连接。
+//子串 "foobar" 开始位置是 9。它是 words 中以 ["foo","bar"] 顺序排列的连接。
+//输出顺序无关紧要。返回 [9,0] 也是可以的。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+//输出：[]
+//解释：因为 words.length == 4 并且 words[i].length == 4，所以串联子串的长度必须为 16。
+//s 中没有子串长度为 16 并且等于 words 的任何顺序排列的连接。
+//所以我们返回一个空数组。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+//输出：[6,9,12]
+//解释：因为 words.length == 3 并且 words[i].length == 3，所以串联子串的长度必须为 9。
+//子串 "foobarthe" 开始位置是 6。它是 words 中以 ["foo","bar","the"] 顺序排列的连接。
+//子串 "barthefoo" 开始位置是 9。它是 words 中以 ["bar","the","foo"] 顺序排列的连接。
+//子串 "thefoobar" 开始位置是 12。它是 words 中以 ["the","foo","bar"] 顺序排列的连接。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// 1 <= words.length <= 5000 
+// 1 <= words[i].length <= 30 
+// words[i] 和 s 由小写英文字母组成 
+// 
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 925 👎 0
+
+package cn.zhanghui.demo.daily.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public List<Integer> findSubstring1(String s, String[] words) {
+        int wlen = words.length, slen = s.length(), ws = words[0].length();
+
+        List<Integer> result = new ArrayList<>();
+        HashMap<String, Integer> allMap = new HashMap<>(words.length);
+        for (String word : words) {
+            int value = allMap.getOrDefault(word, 0) + 1;
+            allMap.put(word, value);
+        }
+
+        out:
+        for (int i = 0; i <= slen - wlen * ws; i++) {
+            HashMap<String, Integer> hasMap = new HashMap<>(words.length);
+            for (int w = 0; w < wlen; w++) {
+                String sWord = s.substring(i + w * ws, i + w * ws + ws);
+                if (!allMap.containsKey(sWord)) {
+                    continue out;
+                }
+                int value = hasMap.getOrDefault(sWord, 0) + 1;
+                if (value > allMap.get(sWord)) {
+                    continue out;
+                }
+                hasMap.put(sWord, value);
+            }
+            result.add(i);
+        }
+        return result;
+    }
+
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        int wordNum = words.length;
+        if (wordNum == 0) {
+            return res;
+        }
+        int wordLen = words[0].length();
+        HashMap<String, Integer> allWords = new HashMap<String, Integer>();
+        for (String w : words) {
+            int value = allWords.getOrDefault(w, 0);
+            allWords.put(w, value + 1);
+        }
+        //将所有移动分成 wordLen 类情况
+        for (int j = 0; j < wordLen; j++) {
+            HashMap<String, Integer> hasWords = new HashMap<String, Integer>();
+            int num = 0; //记录当前 HashMap2（这里的 hasWords 变量）中有多少个单词
+            //每次移动一个单词长度
+            for (int i = j; i < s.length() - wordNum * wordLen + 1; i = i + wordLen) {
+                boolean hasRemoved = false; //防止情况三移除后，情况一继续移除
+                while (num < wordNum) {
+                    String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
+                    if (allWords.containsKey(word)) {
+                        int value = hasWords.getOrDefault(word, 0);
+                        hasWords.put(word, value + 1);
+                        //出现情况三，遇到了符合的单词，但是次数超了
+                        if (hasWords.get(word) > allWords.get(word)) {
+                            // hasWords.put(word, value);
+                            hasRemoved = true;
+                            int removeNum = 0;
+                            //一直移除单词，直到次数符合了
+                            while (hasWords.get(word) > allWords.get(word)) {
+                                String firstWord = s.substring(i + removeNum * wordLen, i + (removeNum + 1) * wordLen);
+                                int v = hasWords.get(firstWord);
+                                hasWords.put(firstWord, v - 1);
+                                removeNum++;
+                            }
+                            num = num - removeNum + 1; //加 1 是因为我们把当前单词加入到了 HashMap 2 中
+                            i = i + (removeNum - 1) * wordLen; //这里依旧是考虑到了最外层的 for 循环，看情况二的解释
+                            break;
+                        }
+                        //出现情况二，遇到了不匹配的单词，直接将 i 移动到该单词的后边（但其实这里
+                        //只是移动到了出现问题单词的地方，因为最外层有 for 循环， i 还会移动一个单词
+                        //然后刚好就移动到了单词后边）
+                    } else {
+                        hasWords.clear();
+                        i = i + num * wordLen;
+                        num = 0;
+                        break;
+                    }
+                    num++;
+                }
+                if (num == wordNum) {
+                    res.add(i);
+
+                }
+                //出现情况一，子串完全匹配，我们将上一个子串的第一个单词从 HashMap2 中移除
+                if (num > 0 && !hasRemoved) {
+                    String firstWord = s.substring(i, i + wordLen);
+                    int v = hasWords.get(firstWord);
+                    hasWords.put(firstWord, v - 1);
+                    num = num - 1;
+                }
+
+            }
+
+        }
+        return res;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
