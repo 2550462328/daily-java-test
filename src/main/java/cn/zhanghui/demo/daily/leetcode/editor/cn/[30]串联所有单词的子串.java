42f73ffbfa8ca7b0101b0a4affd895a1280cb8df
huizhang43 <huizhang43@iflytek.com>
2023-05-18 14:46:46 +0800

    leetcode day and day [2023-05-18]

--- src/main/java/cn/zhanghui/demo/daily/leetcode/editor/cn/[30]串联所有单词的子串.java
+++ src/main/java/cn/zhanghui/demo/daily/leetcode/editor/cn/[30]串联所有单词的子串.java
@@ -62,7 +62,7 @@ import java.util.HashMap;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution30 {
 
     public List<Integer> findSubstring1(String s, String[] words) {
         int wlen = words.length, slen = s.length(), ws = words[0].length();
